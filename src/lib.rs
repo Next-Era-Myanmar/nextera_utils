a204980d96798e3e51c520c510dcd5642f05a4f9
nyan-innovix <nyanlinsoe@innovix-solutions.com>
2024-11-06 16:21:36 +0630

    add response message to models

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+mod models;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn testing_models() {
+        let response = models::response_message::ResponseMessage {
+            message: String::from("Hello"),
+        };
+        assert_eq!(response.message, String::from("Hello"));
+    }
+}
