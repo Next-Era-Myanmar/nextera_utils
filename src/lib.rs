b82fab312a0c0b7d3b83f2a06a1e87c45557cc6e
nyan-innovix <nyanlinsoe@innovix-solutions.com>
2024-11-13 13:41:09 +0630

    add more parser

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod parser;
 
 #[cfg(test)]
 mod tests {
+    use crate::parser::OptionParserExtensions;
     use super::*;
 
     #[test]
@@ -20,14 +21,24 @@ mod tests {
     #[test]
     fn testing_parser() {
         use crate::parser::OptionParserExtensions;
+        use crate::parser::ParserExtensions;
 
         let test: Option<&str> = Some("200");
         let final_result : Option<i32> = Some(200);
-        let result = test.to_i32();
+        let result = test.to_opt_i32();
         assert_eq!(result, final_result);
 
         let test: Option<&str> = Some("hello");
-        let result = test.to_i32();
+        let result = test.to_opt_i32();
+        assert_eq!(result, None);
+
+        let test: String = String::from("200");
+        let final_result : Option<u16> = Some(200);
+        let result = test.to_opt_u16();
+        assert_eq!(result, final_result);
+
+        let test: String = String::from("Hello");
+        let result = test.to_opt_u16();
         assert_eq!(result, None);
     }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,15 +1,57 @@
+//! ## Parser Extensions for Next Era.
+//!
+//! Next Era Solution generic parser are implemented in these modules.
+//!
 pub trait OptionParserExtensions {
-    fn to_i32(self) -> Option<i32>;
+    fn to_opt_i32(self) -> Option<i32>;
 }
 
 impl OptionParserExtensions for Option<&str> {
-    fn to_i32(self) -> Option<i32> {
+    /// ### Parsed form optional immutable str to option i32.
+    /// #### If value contain None or Failed, you will get None. If success you get Option<i32>.
+    ///
+    /// ### Example
+    ///
+    /// ```
+    /// use nextera_utils::parser::OptionParserExtensions;
+    /// let x:Option<&str> = Some("200");
+    /// let y:Option<i32> = Some(200);
+    /// let result = x.to_opt_i32();
+    /// assert_eq!(result, y);
+    /// ```
+    fn to_opt_i32(self) -> Option<i32> {
         match self {
             None => None,
             Some(s) => match s.to_string().parse::<i32>() {
                 Ok(r) => Some(r),
                 Err(_) => None,
             },
+        }
+    }
+}
+
+pub trait ParserExtensions {
+    fn to_opt_u16(self) -> Option<u16>;
+}
+
+impl ParserExtensions for String {
+    /// ### Parsed form String to u16.
+    /// #### If Failed, you will get None. If success you get Option<u16>.
+    ///
+    /// ### Example
+    ///
+    /// ```
+    /// use nextera_utils::parser::ParserExtensions;
+    /// let x:String = String::from("200");
+    /// let result = x.to_opt_u16();
+    ///
+    /// let y:Option<u16> = Some(200);
+    /// assert_eq!(result, y);
+    /// ```
+    fn to_opt_u16(self) -> Option<u16> {
+        match self.parse::<u16>() {
+            Ok(r) => Some(r),
+            Err(_) => None,
         }
     }
 }
